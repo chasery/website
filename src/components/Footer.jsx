0b52ac14051fdcad7be49e8df3c20997838d25af
Ryan <chaserycom@gmail.com>
2017-05-25 09:26:02 -0700

    Retheme and add footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,39 @@ export class Footer extends Component {
         return (
             <div className="Footer">
                 <ul className="Footer-list">
-                    <li className="Footer-item"><a className="Footer-anchor" href="https://www.linkedin.com/in/ryan-chase-75b79813">LinkedIn</a></li>
-                    <li className="Footer-item"><a className="Footer-anchor" href="mailto:ryan@chasery.com">Email</a></li>
-                    <li className="Footer-item"><a className="Footer-anchor" href="tel:503-367-7057">Call</a></li>
+                    <li className="Footer-item">
+                        <a className="Anchor" href="https://www.linkedin.com/in/ryan-chase-75b79813">
+                            <span className="Anchor-icon">
+                                <ReactSVG
+                                    path="/assets/svg/linked-in-icon.svg"
+                                    evalScript="always"
+                                />
+                            </span>
+                            <span className="Anchor-text">LinkedIn</span>
+                        </a>
+                    </li>
+                    <li className="Footer-item">
+                        <a className="Anchor" href="mailto:ryan@chasery.com">
+                            <span className="Anchor-icon">
+                                <ReactSVG
+                                    path="/assets/svg/email-icon.svg"
+                                    evalScript="always"
+                                />
+                            </span>
+                            <span className="Anchor-text">Email</span>
+                        </a>
+                    </li>
+                    <li className="Footer-item">
+                        <a className="Anchor" href="tel:503-367-7057">
+                            <span className="Anchor-icon">
+                                <ReactSVG
+                                    path="/assets/svg/phone-icon.svg"
+                                    evalScript="always"
+                                />
+                            </span>
+                            <span className="Anchor-text">Call</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         );
