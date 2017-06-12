d370df57d33cf556d74352a6fa14c8532cda1c8f
Ryan <chaserycom@gmail.com>
2017-06-11 19:25:49 -0700

    Style work section

--- src/components/DownloadResume.jsx
+++ src/components/DownloadResume.jsx
@@ -5,12 +5,14 @@ export class DownloadResume extends Component {
     render() {
         return (
             <div className="Section">
-                <form method="get" action="./assets/pdf/ryan-chase-resume.pdf">
-                    <button className="Button">
-                        <span className="Button-icon" dangerouslySetInnerHTML={{__html: downloadIcon}} />
-                        <span className="Button-text">Full Resume</span>
-                    </button>
-                </form>
+                <div className="Section-wrapper">
+                    <form method="get" action="./assets/pdf/ryan-chase-resume.pdf">
+                        <button className="Button">
+                            <span className="Button-icon" dangerouslySetInnerHTML={{__html: downloadIcon}} />
+                            <span className="Button-text">Full Resume</span>
+                        </button>
+                    </form>
+                </div>
             </div>
         );
     }
